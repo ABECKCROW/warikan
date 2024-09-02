12815d3bcf4841bc01b17bb4df63f1f34ff4fca1
q-abe <abe@q-co.jp>
2024-09-02 22:12:42 +0900

    Actionの設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/your-repo-name', // リポジトリ名に置き換えてください
+    trailingSlash: true,
+    output: 'export',
+    distDir: 'out',
+};
 
 export default nextConfig;
